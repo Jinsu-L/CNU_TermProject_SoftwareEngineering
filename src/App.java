78b47cd30c56422cb06ad15f137510cd13b324c6
Jinsu Lim <dla0830ld@gmail.com>
2017-11-22 01:29:12 +0900

    DAO 클래스로 분화 javafx로 App 실행할 수 있도록 추가

---
+++ src/App.java
@@ -0,0 +1,20 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("POS");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
