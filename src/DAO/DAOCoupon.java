10737293537e6144227401b7dff02e9448acf046
kdongha <kdongha1597@gmail.com>
2017-11-22 03:36:43 +0900

    Add 4 basic methods to DAOcoupon

--- src/DAO/DAOCoupon.java
+++ src/DAO/DAOCoupon.java
@@ -1,4 +1,31 @@
 package DAO;
 
 public class DAOCoupon {
+
+    private String couponNumber;
+    private int couponAmount;
+
+    public DAOCoupon() {
+    }
+
+    public DAOCoupon(String couponNumber, int couponAmount) {
+        this.couponNumber = couponNumber;
+        this.couponAmount = couponAmount;
+    }
+
+    public String getCouponNumber() {
+        return couponNumber;
+    }
+
+    public void setCouponNumber(String couponNumber) {
+        this.couponNumber = couponNumber;
+    }
+
+    public int getCouponAmount() {
+        return couponAmount;
+    }
+
+    public void setCouponAmount(int couponAmount) {
+        this.couponAmount = couponAmount;
+    }
 }
