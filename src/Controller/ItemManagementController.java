845ef66a01925c2889386b574383c717ff9910fc
Jinsu Lim <dla0830ld@gmail.com>
2017-11-22 17:13:19 +0900

    컨트롤러에 initailizable 인터페이스 구현

--- src/Controller/ItemManagementController.java
+++ src/Controller/ItemManagementController.java
@@ -1,4 +1,13 @@
 package Controller;
 
-public class ItemManagementController {
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ItemManagementController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
+    }
 }
--- src/Controller/ShoppingBasketController.java
+++ src/Controller/ShoppingBasketController.java
@@ -1,4 +1,22 @@
 package Controller;
 
-public class ShoppingBasketController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.awt.event.ActionEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ShoppingBasketController implements Initializable{
+
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        // Button was clicked, do something...
+        System.out.println("ButtonClick!!");
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
