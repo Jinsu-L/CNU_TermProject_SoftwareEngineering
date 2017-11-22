35057a166b1e7e45630c3d72a5ef469b3f52b528
Jinsu Lim <dla0830ld@gmail.com>
2017-11-22 18:20:22 +0900

    관리 버튼 누르면 "ButtonClick" 출렴됨 !! Yeah!!

--- src/Controller/ShoppingBasketController.java
+++ src/Controller/ShoppingBasketController.java
@@ -1,10 +1,10 @@
 package Controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 
-import java.awt.event.ActionEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -41,6 +41,8 @@ public class ShoppingBasketController implements Initializable{
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        itemMangementBtn.setOnAction(event -> handleButtonAction(event));
 
     }
+
 }
