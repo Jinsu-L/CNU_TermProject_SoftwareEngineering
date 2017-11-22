1d658deb74174df4d96f7bb3ae0db4fe9fed4817
Jinsu Lim <dla0830ld@gmail.com>
2017-11-22 15:48:15 +0900

    뷰 정리

--- src/App.java
+++ src/App.java
@@ -11,7 +11,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("View/Main.fxml"));
         Scene scene = new Scene(root);
         primaryStage.setTitle("POS");
         primaryStage.setScene(scene);
