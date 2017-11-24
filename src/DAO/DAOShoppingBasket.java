b862bf1d390e1d6b4416245d9daac33a6502163d
dongha <kdongha1597@gmail.com>
2017-11-24 17:59:36 +0900

    ShoppingBasket 속성, 생성자, getter, setter 생성

--- src/DAO/DAOShoppingBasket.java
+++ src/DAO/DAOShoppingBasket.java
@@ -1,4 +1,17 @@
 package DAO;
 
 public class DAOShoppingBasket {
+    private int shoppingBasketNumber;
+
+    public DAOShoppingBasket(int shoppingBasketNumber) {
+        this.shoppingBasketNumber = shoppingBasketNumber;
+    }
+
+    public int getShoppingBasketNumber() {
+        return shoppingBasketNumber;
+    }
+
+    public void setShoppingBasketNumber(int shoppingBasketNumber) {
+        this.shoppingBasketNumber = shoppingBasketNumber;
+    }
 }
