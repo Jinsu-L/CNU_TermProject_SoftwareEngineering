47213ef1968f442915caa3baeaa67d1209071a80
dongha <kdongha1597@gmail.com>
2017-11-25 09:14:15 +0900

    쿠폰 결제, 카드 결제 상속 추가

--- src/DAO/DAOCreditCardPayment.java
+++ src/DAO/DAOCreditCardPayment.java
@@ -1,4 +1,57 @@
 package DAO;
 
-public class DAOCreditCardPayment {
+public class DAOCreditCardPayment extends DAOPayment {
+    private int paymentNumber;
+    private int paymentAmount;
+    private Type paymentType;
+    private boolean paymentStatus;
+    private String paymentDate;
+    private String cardCompany;
+    private int cardNumber;
+    private int approvalNumber;
+
+    public DAOCreditCardPayment(int paymentNumber, int paymentAmount) {
+        super(paymentNumber, paymentAmount, Type.CARD);
+        this.cardCompany=null;
+        this.cardNumber=-1;
+        this.approvalNumber=-1;
+    }
+
+    public DAOCreditCardPayment(int paymentNumber, int paymentAmount, Type type) {
+        super(paymentNumber, paymentAmount, type);
+        this.cardCompany=null;
+        this.cardNumber=-1;
+        this.approvalNumber=-1;
+    }
+
+    public DAOCreditCardPayment(int paymentNumber, int paymentAmount, Type type, String cardCompany, int cardNumber, int approvalNumber) {
+        super(paymentNumber, paymentAmount, type);
+        this.cardCompany = cardCompany;
+        this.cardNumber = cardNumber;
+        this.approvalNumber = approvalNumber;
+    }
+
+    public String getCardCompany() {
+        return cardCompany;
+    }
+
+    public void setCardCompany(String cardCompany) {
+        this.cardCompany = cardCompany;
+    }
+
+    public int getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(int cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public int getApprovalNumber() {
+        return approvalNumber;
+    }
+
+    public void setApprovalNumber(int approvalNumber) {
+        this.approvalNumber = approvalNumber;
+    }
 }
