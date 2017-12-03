3a2d79d83f95abed9f1c0a54ab0801c5668d6908
Jinsu Lim <dla0830ld@gmail.com>
2017-12-03 22:42:22 +0900

    레이아웃 여러 파일 추가

---
+++ src/Controller/LogoutController.java
@@ -0,0 +1,56 @@
+package Controller;
+
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Optional;
+import java.util.ResourceBundle;
+
+public class LogoutController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public void showLogoutDialog() {
+
+        Optional input = getDialog().showAndWait();
+
+        if (input.isPresent()) {
+            if ((boolean) input.get())
+                System.exit(0);
+        }
+    }
+
+    private Dialog getDialog() {
+        Dialog dialog = new Dialog();
+        FXMLLoader loader = new FXMLLoader(
+                getClass().getResource("/View/Logout.fxml")
+        );
+        try {
+            dialog.setResizable(false);
+            dialog.setDialogPane(loader.load());
+            ButtonType buttonTypeOk = new ButtonType("Yes", ButtonBar.ButtonData.OK_DONE);
+            ButtonType buttonTypeCancel = new ButtonType("No", ButtonBar.ButtonData.CANCEL_CLOSE);
+            dialog.getDialogPane().getButtonTypes().add(buttonTypeOk);
+            dialog.getDialogPane().getButtonTypes().add(buttonTypeCancel);
+
+            dialog.setResultConverter(param -> {
+                if (param == buttonTypeOk) {
+                    return true;
+                }
+                return false;
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return dialog;
+    }
+}
--- src/Controller/ShoppingBasketController.java
+++ src/Controller/ShoppingBasketController.java
@@ -7,11 +7,9 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.GridPane;
-import javafx.stage.Modality;
+import javafx.stage.Popup;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
+
 
 import java.io.IOException;
 import java.net.URL;
@@ -19,37 +17,46 @@ import java.util.ResourceBundle;
 
 /**
  * @author - 임진수
- *
+ * <p>
  * 장바구니 화면에 이벤트 처리를 담당하는 컨트롤러
  * 버튼 종류 :
- *      관리 버튼 - 확장 메뉴 toggle 버튼
- *      상품 관리 - 상품관리 창으로 이동
- *      쿠폰 관리 - 쿠폰발급 모달 동작
- *      매출 현황 - 매출현황 창으로 이동
- *      비밀번호변경 - 비밀번호 변경 모달 동작
- *      로그아웃 - 로그아웃 기능 동작
- *      선택 삭제 - 선택한 물품 삭제
- *      전체 삭제 - 현재 장바구니 내 리스트 전부 삭제
- *      결제 - 결제 기능 모달 동작
- *      적용 - 텍스트 박스에 숫자를 입력 받아서 선택한 상품에 수량을 변경
- *
+ * 관리 버튼 - 확장 메뉴 toggle 버튼
+ * 상품 관리 - 상품관리 창으로 이동
+ * 쿠폰 관리 - 쿠폰발급 모달 동작
+ * 매출 현황 - 매출현황 창으로 이동
+ * 비밀번호변경 - 비밀번호 변경 모달 동작
+ * 로그아웃 - 로그아웃 기능 동작
+ * 선택 삭제 - 선택한 물품 삭제
+ * 전체 삭제 - 현재 장바구니 내 리스트 전부 삭제
+ * 결제 - 결제 기능 모달 동작
+ * 적용 - 텍스트 박스에 숫자를 입력 받아서 선택한 상품에 수량을 변경
+ * <p>
  * 상품 리스트 클릭 - 해당 상품 포커스
- *
+ * <p>
  * 카테고니 내 상품 클릭 - 각 해당하는 상품을 장바구니에 하나씩 등록
  */
 
-public class ShoppingBasketController implements Initializable{
-    @FXML private Button itemMangementBtn;
-    @FXML private Button mangementBtn;
-    @FXML private Button couponBtn;
-    @FXML private Button salesStatusBtn;
-    @FXML private Button pwChgBtn;
-    @FXML private Button logoutBtn;
-    @FXML private Button slcDelBtn;
-    @FXML private Button allDelBtn;
-    @FXML private Button payBtn;
-    @FXML private Button applyBtn;
-    @FXML private AnchorPane ap;
+public class ShoppingBasketController implements Initializable {
+    @FXML
+    private Button itemMangementBtn;
+    @FXML
+    private Button mangementBtn;
+    @FXML
+    private Button couponBtn;
+    @FXML
+    private Button salesStatusBtn;
+    @FXML
+    private Button pwChgBtn;
+    @FXML
+    private Button logoutBtn;
+    @FXML
+    private Button slcDelBtn;
+    @FXML
+    private Button allDelBtn;
+    @FXML
+    private Button payBtn;
+    @FXML
+    private Button applyBtn;
 
     @FXML
     private void itemManagementButtonAction(ActionEvent event) {
@@ -100,6 +107,8 @@ public class ShoppingBasketController implements Initializable{
     @FXML
     private void logoutButtonAction(ActionEvent event) {
         System.out.println("logoutBtn");
+        LogoutController logoutController = new LogoutController();
+        logoutController.showLogoutDialog();
     }
 
     @FXML
