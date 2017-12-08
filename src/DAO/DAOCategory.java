9018529782e018a6720787fd52494ffb962bcc70
dongha <kdongha1597@gmail.com>
2017-12-09 01:26:19 +0900

    카테고리, 상품 메소드 완료

--- src/DAO/DAOCategory.java
+++ src/DAO/DAOCategory.java
@@ -2,6 +2,7 @@ package DAO;
 
 import DBCP.ConnectionManager;
 import java.sql.*;
+import java.util.Vector;
 
 public class DAOCategory {
     private int categoryID;
@@ -57,7 +58,70 @@ public class DAOCategory {
         return categoryName;
     }
 
+    public String getCategoryName(int categoryID){
+        String result="";
+        Connection conn = null;
+        PreparedStatement pstmt=null;
+        ResultSet rs =null;
+        String query = "SELECT category_name FROM category WHERE categoryID='"+categoryID+"'";
+        try {
+            ConnectionManager cm = new ConnectionManager();
+            conn = cm.getConnection();
+            pstmt = conn.prepareStatement(query);
+            rs = pstmt.executeQuery();
+            rs.next();
+            result=rs.getString("category_name");
+            rs.close();
+            pstmt.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            if(rs != null) try { rs.close(); } catch(Exception e) {}
+            if(pstmt != null) try { pstmt.close(); } catch(Exception e) {}
+            if(conn != null) try { conn.close(); } catch(Exception e) {}
+        }
+        return result;
+    }
+
     public void setCategoryName(String categoryName) {
         this.categoryName = categoryName;
+    }
+
+    //카테고리 내역 조회
+    public Vector<DAOCategory> getCategories(){
+        Vector<DAOCategory> result = new Vector<>();
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        String query = "SELECT * FROM item";
+        try {
+            ConnectionManager cm = new ConnectionManager();
+            conn = cm.getConnection();
+            pstmt = conn.prepareStatement(query);
+            rs = pstmt.executeQuery();
+            while (rs.next()) {
+                int rsCategoryID=rs.getInt("categoryID");
+                String rsCategoryName=rs.getString("category_name");
+                result.add(new DAOCategory(rsCategoryID,rsCategoryName));
+            }
+            rs.close();
+            pstmt.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (rs != null) try {
+                rs.close();
+            } catch (Exception e) {
+            }
+            if (pstmt != null) try {
+                pstmt.close();
+            } catch (Exception e) {
+            }
+            if (conn != null) try {
+                conn.close();
+            } catch (Exception e) {
+            }
+        }
+        return result;
     }
 }
--- src/DAO/DAOItem.java
+++ src/DAO/DAOItem.java
@@ -10,14 +10,18 @@ public class DAOItem {
     private int itemPrice;
     private DAOCategory daoCategory;
 
-    public DAOItem(){
-        this.daoCategory=new DAOCategory();
+    public DAOItem() {
+        this.daoCategory = new DAOCategory();
+    }
+
+    public DAOItem(String itemName) {
+        this.itemName = itemName;
     }
 
     public DAOItem(String itemName, int itemPrice, String categoryName) {
         this.itemName = itemName;
         this.itemPrice = itemPrice;
-        this.daoCategory=new DAOCategory(categoryName);
+        this.daoCategory = new DAOCategory(categoryName);
     }
 
     public DAOItem(String itemName, int itemPrice, DAOCategory daoCategory) {
@@ -29,9 +33,9 @@ public class DAOItem {
     public String getItemName() {
         return itemName;
     }
-    
+
     public void setItemName(String itemName) {
-        this.itemName=itemName;
+        this.itemName = itemName;
     }
 
     public int getItemPrice() {
@@ -50,32 +54,198 @@ public class DAOItem {
         this.daoCategory = daoCategory;
     }
 
-    public Vector<DAOItem> getItems(String categoryName){
-        Vector<DAOItem> result=new Vector<>();
+    //상품 내역 조회
+    public Vector<DAOItem> getItems() {
+        Vector<DAOItem> result = new Vector<>();
         Connection conn = null;
-        PreparedStatement pstmt=null;
-        ResultSet rs =null;
-        DAOCategory rsDAOCategory=new DAOCategory(getDaoCategory().getCategoryID(categoryName),categoryName);
-        String query = "SELECT * FROM item WHERE categoryID='"+rsDAOCategory.getCategoryID()+"'";
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        String query = "SELECT * FROM item";
         try {
             ConnectionManager cm = new ConnectionManager();
             conn = cm.getConnection();
             pstmt = conn.prepareStatement(query);
             rs = pstmt.executeQuery();
-            while(rs.next()) {
-                String rsItemName=rs.getString("item_name");
-                int rsItemPrice=rs.getInt("item_price");
+            while (rs.next()) {
+                String rsItemName = rs.getString("item_name");
+                int rsItemPrice = rs.getInt("item_price");
+                int rsCategoryID=rs.getInt("categoryID");
+                DAOCategory rsDAOCategory=new DAOCategory(rsCategoryID,daoCategory.getCategoryName(rsCategoryID));
                 result.add(new DAOItem(rsItemName,rsItemPrice,rsDAOCategory));
             }
             rs.close();
             pstmt.close();
         } catch (Exception e) {
             e.printStackTrace();
-        }finally {
-            if(rs != null) try { rs.close(); } catch(Exception e) {}
-            if(pstmt != null) try { pstmt.close(); } catch(Exception e) {}
-            if(conn != null) try { conn.close(); } catch(Exception e) {}
+        } finally {
+            if (rs != null) try {
+                rs.close();
+            } catch (Exception e) {
+            }
+            if (pstmt != null) try {
+                pstmt.close();
+            } catch (Exception e) {
+            }
+            if (conn != null) try {
+                conn.close();
+            } catch (Exception e) {
+            }
         }
         return result;
+    }
+
+    //해당 카테고리 상품 요청청
+    public Vector<DAOItem> getItems(String categoryName) {
+        Vector<DAOItem> result = new Vector<>();
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        DAOCategory rsDAOCategory = new DAOCategory(getDaoCategory().getCategoryID(categoryName), categoryName);
+        String query = "SELECT * FROM item WHERE categoryID='" + rsDAOCategory.getCategoryID() + "'";
+        try {
+            ConnectionManager cm = new ConnectionManager();
+            conn = cm.getConnection();
+            pstmt = conn.prepareStatement(query);
+            rs = pstmt.executeQuery();
+            while (rs.next()) {
+                String rsItemName = rs.getString("item_name");
+                int rsItemPrice = rs.getInt("item_price");
+                result.add(new DAOItem(rsItemName, rsItemPrice, rsDAOCategory));
+            }
+            rs.close();
+            pstmt.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (rs != null) try {
+                rs.close();
+            } catch (Exception e) {
+            }
+            if (pstmt != null) try {
+                pstmt.close();
+            } catch (Exception e) {
+            }
+            if (conn != null) try {
+                conn.close();
+            } catch (Exception e) {
+            }
+        }
+        return result;
+    }
+
+    //상품 등록 요청
+    public void insertItem(String itemName, int itemPrice, String categoryName) {
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        DAOCategory rsDAOCategory = new DAOCategory(getDaoCategory().getCategoryID(categoryName), categoryName);
+        String query = "INSERT INTO item VALUES (" + itemName + "," + itemPrice + "," + rsDAOCategory.getCategoryID() + ")";
+        try {
+            ConnectionManager cm = new ConnectionManager();
+            conn = cm.getConnection();
+            pstmt = conn.prepareStatement(query);
+            pstmt.executeUpdate();
+            pstmt.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (pstmt != null) try {
+                pstmt.close();
+            } catch (Exception e) {
+            }
+            if (conn != null) try {
+                conn.close();
+            } catch (Exception e) {
+            }
+        }
+    }
+
+    //상품 정보 요청
+    public DAOItem getItemDetail(String itemName) {
+        DAOItem result = new DAOItem(itemName);
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        String query = "SELECT * FROM item WHERE item_name='" + itemName + "'";
+        try {
+            ConnectionManager cm = new ConnectionManager();
+            conn = cm.getConnection();
+            pstmt = conn.prepareStatement(query);
+            rs = pstmt.executeQuery();
+            rs.next();
+            result.setItemPrice(rs.getInt("item_price"));
+            int rsCategoryID = rs.getInt("categoryID");
+            result.setDaoCategory(new DAOCategory(rsCategoryID, daoCategory.getCategoryName(rsCategoryID)));
+            rs.close();
+            pstmt.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (rs != null) try {
+                rs.close();
+            } catch (Exception e) {
+            }
+            if (pstmt != null) try {
+                pstmt.close();
+            } catch (Exception e) {
+            }
+            if (conn != null) try {
+                conn.close();
+            } catch (Exception e) {
+            }
+        }
+        return result;
+    }
+
+    //상품 삭제 요청
+    public void deleteItem(String itemName) {
+        DAOItem result = new DAOItem(itemName);
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        String query = "DELETE FROM item WHERE item_name='" + itemName + "'";
+        try {
+            ConnectionManager cm = new ConnectionManager();
+            conn = cm.getConnection();
+            pstmt = conn.prepareStatement(query);
+            pstmt.executeUpdate();
+            pstmt.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (pstmt != null) try {
+                pstmt.close();
+            } catch (Exception e) {
+            }
+            if (conn != null) try {
+                conn.close();
+            } catch (Exception e) {
+            }
+        }
+    }
+
+    //상품 수정 요청
+    public void updateItem(String itemName, String newItemName, int newItemPrice, String newCategoryName) {
+        DAOItem result = new DAOItem(itemName);
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        int newCategoryID = daoCategory.getCategoryID(newCategoryName);
+        String query = "UPDATE FROM item SET item_name='" + newItemName + "', item_price =" + newItemPrice + ", categoryID=" + newCategoryID + "WHERE item_name='" + itemName + "'";
+        try {
+            ConnectionManager cm = new ConnectionManager();
+            conn = cm.getConnection();
+            pstmt = conn.prepareStatement(query);
+            pstmt.executeUpdate();
+            pstmt.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (pstmt != null) try {
+                pstmt.close();
+            } catch (Exception e) {
+            }
+            if (conn != null) try {
+                conn.close();
+            } catch (Exception e) {
+            }
+        }
     }
 }
