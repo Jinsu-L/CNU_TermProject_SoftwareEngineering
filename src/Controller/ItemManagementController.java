f4da1a0092d9944d461d8836eb12dc205fc91618
Jinsu Lim <dla0830ld@gmail.com>
2017-12-14 22:39:05 +0900

    itemManagement view List implement

--- src/Controller/ItemManagementController.java
+++ src/Controller/ItemManagementController.java
@@ -1,5 +1,12 @@
 package Controller;
 
+import DAO.DAOItem;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -7,10 +14,13 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 /**
@@ -20,10 +30,14 @@ import java.util.ResourceBundle;
 /* Todo 아이템 관리 기능 추가 */
 
 public class ItemManagementController implements Initializable {
-    @FXML private Button backBtn;
-    @FXML private Button registerBtn;
-    @FXML private Button modifyBtn;
-    @FXML private Button deleteBtn;
+    @FXML
+    private Button backBtn;
+    @FXML
+    private Button registerBtn;
+    @FXML
+    private Button modifyBtn;
+    @FXML
+    private Button deleteBtn;
 
     @FXML
     private void backButtonAction(ActionEvent event) {
@@ -59,5 +73,52 @@ public class ItemManagementController implements Initializable {
         registerBtn.setOnAction(this::registerButtonAction);
         modifyBtn.setOnAction(this::modifyButtonAction);
         deleteBtn.setOnAction(this::deleteButtonAction);
+        loadItemList();
+    }
+
+    ObservableList<TableRowDataModel> itemList = FXCollections.observableArrayList();
+    @FXML
+    private TableView<TableRowDataModel> itemListView;
+    @FXML
+    private TableColumn<TableRowDataModel, String> itemName;
+    @FXML
+    private TableColumn<TableRowDataModel, String> category;
+    @FXML
+    private TableColumn<TableRowDataModel, Integer> price;
+
+    public void loadItemList() {
+        ArrayList<DAOItem> itemArrayList = new DAOItem().getItems();
+        for (DAOItem item : itemArrayList) {
+            itemList.add(new TableRowDataModel(new SimpleStringProperty(item.getItemName()),new SimpleStringProperty(item.getDaoCategory().getCategoryName()),new SimpleIntegerProperty(item.getItemPrice())));
+        }
+
+        itemName.setCellValueFactory(cellData -> cellData.getValue().itemNameProperty());
+        category.setCellValueFactory(cellData -> cellData.getValue().categoryProperty());
+        price.setCellValueFactory(cellData -> cellData.getValue().priceProperty().asObject());
+        itemListView.setItems(itemList);
+    }
+
+    public class TableRowDataModel {
+        private StringProperty itemName;
+        private StringProperty category;
+        private IntegerProperty price;
+
+        public TableRowDataModel(StringProperty name, StringProperty category, IntegerProperty price) {
+            this.itemName = name;
+            this.category = category;
+            this.price = price;
+        }
+
+        public StringProperty itemNameProperty() {
+            return itemName;
+        }
+
+        public StringProperty categoryProperty() {
+            return category;
+        }
+
+        public IntegerProperty priceProperty() {
+            return price;
+        }
     }
 }
