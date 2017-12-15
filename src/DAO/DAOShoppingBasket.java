16a673ac28b27d5cf65e30135e8013c52ef67ae4
kdongha <kdongha1597@gmail.com>
2017-12-15 14:30:22 +0900

    getBasketSize

--- src/DAO/DAOShoppingBasket.java
+++ src/DAO/DAOShoppingBasket.java
@@ -1,5 +1,8 @@
 package DAO;
 
+import DBCP.ConnectionManager;
+
+import java.sql.*;
 import java.util.ArrayList;
 
 public class DAOShoppingBasket {
@@ -7,10 +10,7 @@ public class DAOShoppingBasket {
     private ArrayList<DAOShoppingHistory> daoShoppingHistories;
     private DAOShoppingHistory daoShoppingHistory;
 
-    static private int shoppingBasketCount = 0;
-
     public DAOShoppingBasket() {
-        this.shoppingBasketNumber = shoppingBasketCount++;
         daoShoppingHistories = new ArrayList<>();
         daoShoppingHistory = new DAOShoppingHistory();
     }
@@ -44,14 +44,6 @@ public class DAOShoppingBasket {
         this.daoShoppingHistory = daoShoppingHistory;
     }
 
-    public static int getShoppingBasketCount() {
-        return shoppingBasketCount;
-    }
-
-    public static void setShoppingBasketCount(int shoppingBasketCount) {
-        DAOShoppingBasket.shoppingBasketCount = shoppingBasketCount;
-    }
-
     //상품 담기
     public ArrayList<DAOShoppingHistory> insertHistory(int shoppingBasketNumber, String itemName) {
         return daoShoppingHistory.insertHistory(shoppingBasketNumber, itemName);
@@ -70,5 +62,39 @@ public class DAOShoppingBasket {
     //상품 전체 삭제
     public ArrayList<DAOShoppingHistory> deleteHistory(int shoppingBasketNumber) {
         return daoShoppingHistory.deleteHistory(shoppingBasketNumber);
+    }
+
+    public int getBasketSize(){
+        int size=-1;
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        String query = "SELECT * FROM shopping_basket";
+        try {
+            ConnectionManager cm = new ConnectionManager();
+            conn = cm.getConnection();
+            pstmt = conn.prepareStatement(query);
+            rs = pstmt.executeQuery();
+            rs.last();
+            size=rs.getRow();
+            rs.close();
+            pstmt.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (rs != null) try {
+                rs.close();
+            } catch (Exception e) {
+            }
+            if (pstmt != null) try {
+                pstmt.close();
+            } catch (Exception e) {
+            }
+            if (conn != null) try {
+                conn.close();
+            } catch (Exception e) {
+            }
+        }
+        return size;
     }
 }
