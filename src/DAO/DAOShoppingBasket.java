58425980f829de65ce9ee65795a62363d6b69eee
kdongha <kdongha1597@gmail.com>
2017-12-15 14:23:43 +0900

    getDaoShoppingHistories

--- src/DAO/DAOShoppingBasket.java
+++ src/DAO/DAOShoppingBasket.java
@@ -17,6 +17,7 @@ public class DAOShoppingBasket {
 
     public DAOShoppingBasket(int shoppingBasketNumber) {
         this.shoppingBasketNumber = shoppingBasketNumber;
+        this.daoShoppingHistories=getDaoShoppingHistory().getShoppingHistories(this.getShoppingBasketNumber());
     }
 
     public int getShoppingBasketNumber() {
@@ -25,6 +26,30 @@ public class DAOShoppingBasket {
 
     public void setShoppingBasketNumber(int shoppingBasketNumber) {
         this.shoppingBasketNumber = shoppingBasketNumber;
+    }
+
+    public ArrayList<DAOShoppingHistory> getDaoShoppingHistories() {
+        return daoShoppingHistories;
+    }
+
+    public void setDaoShoppingHistories(ArrayList<DAOShoppingHistory> daoShoppingHistories) {
+        this.daoShoppingHistories = daoShoppingHistories;
+    }
+
+    public DAOShoppingHistory getDaoShoppingHistory() {
+        return daoShoppingHistory;
+    }
+
+    public void setDaoShoppingHistory(DAOShoppingHistory daoShoppingHistory) {
+        this.daoShoppingHistory = daoShoppingHistory;
+    }
+
+    public static int getShoppingBasketCount() {
+        return shoppingBasketCount;
+    }
+
+    public static void setShoppingBasketCount(int shoppingBasketCount) {
+        DAOShoppingBasket.shoppingBasketCount = shoppingBasketCount;
     }
 
     //상품 담기
